38bb87fc71d5ac5a92835629afae5f508afffa10
abelardogg <abelardogy@gmail.com>
2019-02-01 10:52:07 -0500

    Entities Rate User

---
+++ src/main/java/com/aca/talks/domain/User.java
@@ -0,0 +1,27 @@
+package com.aca.talks.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @OneToMany
+    private List<Talk> talkList = new ArrayList<>();
+
+    @OneToMany
+    private List<Ratings> rates = new ArrayList<>();
+
+
+
+}
