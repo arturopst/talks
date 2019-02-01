0afa43ce758cea6044aea2099e142ff3d485f5a4
abelardogg <abelardogy@gmail.com>
2019-02-01 14:47:02 -0500

    landing table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO Talk(ID, Name, Description, Url) VALUES (1,'JavaScript', 'Mucho JS', 'www.google.com');
+INSERT INTO Talk(ID, Name, Description, Url) VALUES (2,'Spring', 'Mucho Java', 'www.google.com');
+INSERT INTO Talk(ID, Name, Description, Url) VALUES (3,'Perritos de la pradera', 'El maravilloso mundo de perritos de la pradera', 'www.google.com');
